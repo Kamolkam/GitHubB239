e5a1eb4d1e0b186b6c98020143b10e874d4e9b79
Kamola Ortikova <email>
2024-03-10 11:26:02 -0400

    v2

--- src/Class01.java
+++ src/Class01.java
@@ -2,6 +2,9 @@ public class Class01 {
      public static void main(String[] args) {
 
          //Version 1
-         System.out.ptintln("This is a code for version 1");
-    }
+         System.out.println("This is a code for version 1");
+
+         //Version 2
+         System.out.println("This is a code for version 2");
+     }
 }
